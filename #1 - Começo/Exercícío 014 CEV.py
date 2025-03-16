4d56965c3c14fc6cd6eab943024cb07eac4941c7
Kauan <159711116+WillsKFG@users.noreply.github.com>
2025-03-15 21:48:06 -0300

    vamos la

---
+++ #1 - Começo/Exercícío 014 CEV.py
@@ -0,0 +1,5 @@
+#conversor graus celsius para fahrenheit
+c = float(input('Informe a temperatura em graus Celsius: '))
+f = c * 1,8 + 32
+
+print('A temperatura de {}°C corresponde a {}°F'.format(c, f))
