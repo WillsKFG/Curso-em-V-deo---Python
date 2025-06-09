4caa395ba0e63c80492db275314f1a45a9bef8e1
Kauan <159711116+WillsKFG@users.noreply.github.com>
2025-06-08 23:35:17 -0300

    05/05/25

---
+++ #5 - Cores no Terminal/cores_terminal.py
@@ -0,0 +1,13 @@
+  #  text                  background
+ 
+#30    black             preto       40
+#31    red               vermelho    41
+#32    green             verde       42
+#33    yellow            amarelo     43
+#34    blue              azul        44
+#35    magenta           magenta     45
+#36    cyan              ciano       46
+#37    grey              cinza       47
+#97    white             branco      107
+
+print("\033[0;35;43mkauan")
